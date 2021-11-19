56e67a18f002181d4ef1dfcdf92e606c4d65da1b
oghusky <philiposimmons@gmail.com>
2021-11-18 20:07:04 -0500

    added lines to server to render react build

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // test route
 app.get("/config", (req, res) => {
@@ -29,6 +29,11 @@ app.use('/auth', require('./routes/auth-routes'));
 app.use('/question', require('./routes/question-routes'));
 app.get("/*", (req, res) => {
     return res.status(404).json({ msg: "Can not find what you are looking for" });
+});
+
+// index html route
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'build/index.html'));
 })
 
 app.listen(PORT, () => {
