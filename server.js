c89a522e97d4a330a007d6ee5d2ee5cce9124973
oghusky <philiposimmons@gmail.com>
2021-11-18 20:11:38 -0500

    removed error route from server file

--- server.js
+++ server.js
@@ -27,9 +27,6 @@ app.get("/config", (req, res) => {
 app.use("/", require('./routes/public-routes'));
 app.use('/auth', require('./routes/auth-routes'));
 app.use('/question', require('./routes/question-routes'));
-app.get("/*", (req, res) => {
-    return res.status(404).json({ msg: "Can not find what you are looking for" });
-});
 
 // index html route
 app.get("*", (req, res) => {
